0233f9eb23436d0dee3f650583ed9a3175b2935d
Brian Graham <graham.briank@gmail.com>
2015-09-02 20:39:54 -0700

    implement myTunes basic functionality

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,28 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
-
+  
   initialize: function() {
+    this.on('dequeue', this.dequeue, this);
+    this.on('add', this.checkLength, this);
+    this.on('ended', this.dequeue, this);
+  },
+
+  playFirst: function () {
+    
+    this.models[0].play();
+  },
+  
+  checkLength: function () {
+    if (this.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  dequeue: function () {
+    this.shift();
+    if (this.models[0]) {
+      this.playFirst();
+    }
   }
 
 });
